d011113fe422d0bcb7030c82a672ca08f9bbfebc
Luke Stahl <Stahl1109@gmail.com>
2018-03-05 15:07:20 -0600

    survey.html working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "..public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     })
 
     // If no matching route is found default to home
