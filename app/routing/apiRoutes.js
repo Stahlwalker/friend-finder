3d438b9074bb0a2000bdbab8f44e6b68de27da1c
Luke Stahl <Stahl1109@gmail.com>
2018-03-09 07:08:04 -0600

    working on making FriendsArray all strings

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,21 @@
-
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-
 var friendsData = require("../data/friends");
-
-
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
-
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
@@ -31,7 +23,6 @@ module.exports = function(app) {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
@@ -41,21 +32,20 @@ module.exports = function(app) {
       friendsData.push(req.body);
       res.json(true);
     }
-
     // finalscore = 1 +2
     // finalscore = parseInt(person1score) + parseInt(person1score)
-
-
+    // var newFreindSCores = req.body.scores;
+    // var scoresArray = [];
+    // var friendCount = 0;
+    // var bestMatch = 0;
     // var match = {
     //   name: "",
     //   photo: "",
     //   difference: 0
     // }
-
     // function sum(scores){
     //   if (toString.call(scores) !=="[friendsArray]")
     //   return false;
-
     //     var total = 0;
     //     for(var i=0; i<scores.length; i++)
     //     {
@@ -66,28 +56,21 @@ module.exports = function(app) {
     //     }
     // }
     // console.log(sum([]));
-
     // var totalDifference = {
-
     // }
 // Loop through all the data
 // if else statement
-
-
     // else {
     //   waitListData.push(req.body);
     //   res.json(false);
     // }
   });
-
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
-
   // app.post("/api/clear", function() {
   //   // Empty out the arrays of data
   //   friendsData = [];
-
   //   console.log(friendsData);
   // });
 };
